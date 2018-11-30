10c969fddb90ddc8811daff0409ecb8218f46f11
LilKoke <44338851+LilKoke@users.noreply.github.com>
2018-11-30 11:36:39 +0900

    マクロの変更とチートシート

--- template.cpp
+++ template.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 #define print(s) cout << (s) << endl;
 #define sp(x) cout<<setprecision(x);
-
+#define rep(i,n)  for (int i = 0; i < (n); i++)
+#define all(a) a.begin(), (a).end()
+#define gre(t) greater<t>()
+ 
 int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
 }
